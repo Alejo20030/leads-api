8c78c943481fa303d279d3d63bcf81c53a9aadc0
Alejo20030 <gonzajuan2003@gmail.com>
2026-05-18 16:57:18 -0500

    Initial NestJS project setup

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+import { LeadsModule } from './leads/leads.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRoot(process.env.MONGO_URI),
+
+    LeadsModule,
+  ],
+})
+export class AppModule {}
